e50740e8148f16d46c98e906488d30b5e59265bf
Sona Salehian <sona.salehianghamsari@uni.lu>
2023-12-13 13:18:12 +0100

    works in HPC

--- launcher_parallel.sh
+++ launcher_parallel.sh
@@ -1,20 +1,7 @@
-# #!/bin/bash
-
-# rm hpc_output/result.txt
-
-# python3 random_parameters.py
-# file_path = 'hpc_output/random_values.txt' | python3 statistical_analysis.py
-
-# cat hpc_output/random_values.txt | parallel python3 function.py {}
-# echo Results are calculated
-
-# file_path = 'hpc_output/result.txt' | python3 statistical_analysis.py
-
-
 #!/bin/bash
 
 # Remove existing result.txt
-# rm hpc_output/result.txt
+rm hpc_output/result.txt
 
 # Generate random parameters
 python3 random_parameters.py
@@ -29,9 +16,6 @@ python3 statistical_analysis.py hpc_output/random_values.txt
 cat "$file_path" | parallel python3 function.py {}
 
 echo "Results are calculated"
-
-# Set the file path for result.txt
-# file_path='hpc_output/result.txt'
 
 # Run statistical analysis for the result
 python3 statistical_analysis.py hpc_output/result.txt
