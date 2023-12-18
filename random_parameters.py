23bfbd75ad3a795cd937f9b4ed0904dbf5aba0d9
Sona Salehian <sona.salehianghamsari@uni.lu>
2023-12-18 12:38:19 +0100

    update to try with model

--- random_parameters.py
+++ random_parameters.py
@@ -1,10 +1,24 @@
 from scipy.stats import lognorm
 import os
+import numpy as np
+
+def stretch_to_range(values, new_min, new_max):
+    # Assuming values are between 0.7 and 1.4
+    old_min, old_max = np.min(values), np.max(values)
+    
+    # Perform linear transformation to stretch the values to the new range
+    stretched_values = (values - old_min) / (old_max - old_min) * (new_max - new_min) + new_min
+    
+    return stretched_values
 
 # Generate random values with lognorm
-sigma = 1.2 # standard deviation
+sigma = 0.1 # standard deviation
 
-x = lognorm.rvs(sigma, size=10000)
+x = lognorm.rvs(sigma, size=2)
+lower_limit = 4.7E-13
+upper_limit = 1.1E-11
+
+hc = stretch_to_range(x, lower_limit, upper_limit)
 
 # Save the random values
 file_name = 'hpc_output/random_values.txt'
@@ -15,7 +29,7 @@ else:
   print("The file does not exist")
 
 with open(file_name, 'w') as file:
-    for number in x:
+    for number in hc:
         file.write(f"{number}\n")
 
 print(f"Random values have been saved to {file_name}")
--- statistical_analysis.py
+++ statistical_analysis.py
@@ -4,8 +4,10 @@ import sys
 
 
 def calculate_statistics(data):
+    min = np.min(data)
+    max = np.max(data)
     mean = np.mean(data)
-    mode = stats.mode(data, keepdims=True).mode[0]
+    # mode = stats.mode(data, keepdims=True).mode[0]
     median = np.median(data)
     data_range = np.ptp(data)
     quartiles = np.percentile(data, [25, 50, 75])
@@ -13,7 +15,7 @@ def calculate_statistics(data):
     variance = np.var(data)
     std_deviation = np.std(data)
     
-    return mean, mode, median, data_range, quartiles, iqr, variance, std_deviation
+    return min, max, mean, median, data_range, quartiles, iqr, variance, std_deviation
 
 
 def read_numbers_from_file(file_path):
@@ -26,10 +28,12 @@ def read_numbers_from_file(file_path):
 def statistical_analysis(file_path):    
     results = read_numbers_from_file(file_path)
     
-    mean, mode, median, data_range, quartiles, iqr, variance, std_deviation = calculate_statistics(results)
+    min, max, mean, median, data_range, quartiles, iqr, variance, std_deviation = calculate_statistics(results)
     
+    print(f"Min: {min}")
+    print(f"Max: {max}")
     print(f"Mean: {mean}")
-    print(f"Mode: {mode}")
+    # print(f"Mode: {mode}")
     print(f"Median: {median}")
     print(f"Range: {data_range}")
     print(f"Quartiles: Q1 = {quartiles[0]}, Q2 = {quartiles[1]}, Q3 = {quartiles[2]}")
