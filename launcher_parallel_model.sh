decb55acc37b1c86309e0d6a0dfee32dd8038a3f
Sona Salehian Ghamsari sona.salehianghamsari@uni.lu <ssalehian@access2.iris-cluster.uni.lux>
2024-01-09 14:31:07 +0100

    112 cores in bigmem

--- launcher_parallel_model.sh
+++ launcher_parallel_model.sh
@@ -4,7 +4,7 @@
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
 #SBATCH --time=07:00:00
-#SBATCH --ntasks-per-node=28
+#SBATCH --ntasks-per-node=112
 #SBATCH -p bigmem
 
 ## Command(s) to run (example):
@@ -25,5 +25,5 @@ echo "bigmem, n=4, t=7:00:00"
 
 # parallel --jobs $JOBS_PER_NODE --slf hostfile --wd $WDIR --joblog task.log --resume --progress -a task.lst sh run-blast.sh {} output/{/.}.blst $SLURM_CPUS_PER_TASK
 
-parallel --jobs $JOBS_PER_NODE srun -n 4 -c 1 python3 random_ahc.py {} ::: {6..54}
+parallel --jobs $JOBS_PER_NODE srun -n 4 -c 1 python3 random_ahc.py {} ::: {55..110}
 
