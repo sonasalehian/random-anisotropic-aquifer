dafed48e083c7a001a1259e99ed9516f4c512bd7
Sona Salehian Ghamsari sona.salehianghamsari@uni.lu <ssalehian@access2.iris-cluster.uni.lux>
2024-02-14 14:53:47 +0100

    spack environment

--- launcher_parallel_model.sh
+++ launcher_parallel_model.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#SBATCH --job-name=my_parallel_job
+#SBATCH --job-name=spack_parallel
 #SBATCH --output=logs/%x-%j.out
 #SBATCH --nodes=1
 #SBATCH --cpus-per-task=1
@@ -10,7 +10,10 @@
 ## Command(s) to run (example):
 # module load gnu-parallel/2019.03.22
 
-source ${SCRATCH}fenicsx-iris-gompi-32-20230116-r2/bin/env-fenics.sh
+# source ${SCRATCH}fenicsx-iris-gompi-32-20230116-r2/bin/env-fenics.sh
+
+source $SCRATCH/spack/share/spack/setup-env.sh
+spack env activate fenicsx-main-20230214
 
 export WDIR=$SCRATCH/stochastic_model
 cd $WDIR
@@ -18,7 +21,7 @@ cd $WDIR
 # set number of jobs based on number of cores available and number of threads per job
 export JOBS_PER_NODE=$(( $SLURM_CPUS_ON_NODE / $SLURM_CPUS_PER_TASK ))
 
-echo "fenicsx-iris-gompi-32-20230116-r2"
+echo "spack env: fenicsx-main-20230214"
 echo $SLURM_CPUS_ON_NODE
 echo $SLURM_CPUS_PER_TASK
 echo $JOBS_PER_NODE 
@@ -26,5 +29,5 @@ echo "batch, n=8, c=1, t=4:00:00,0..2"
 
 # parallel --jobs $JOBS_PER_NODE --slf hostfile --wd $WDIR --joblog task.log --resume --progress -a task.lst sh run-blast.sh {} output/{/.}.blst $SLURM_CPUS_PER_TASK
 
-parallel --jobs $JOBS_PER_NODE srun -n 8 -c 1 python3 random_ahc.py {} ::: {0..2}
+parallel --jobs $JOBS_PER_NODE srun -n 8 -c 1 python3 random_ahc_tensor.py {} ::: {0..2}
 
