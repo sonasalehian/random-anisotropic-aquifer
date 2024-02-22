968fece14815b262159e6e10389b48aded45a2dc
Sona Salehian <sona.salehianghamsari@uni.lu>
2024-02-22 17:53:51 +0100

    arithmetic on coefficients

--- statistical_analysis_checkpoints.py
+++ statistical_analysis_checkpoints.py
@@ -20,7 +20,7 @@ T2 = parameters["T2"]
 num_steps2 = parameters["num_steps2"]
 dt2 = (T2-T) / num_steps2
 
-n_outputs = 10
+n_outputs = 4
 n = 0
 filename = f'./output/random_s_los/random_ahc_{n}/submesh_checkpoint.bp'
 engine = "BP4"
@@ -28,41 +28,42 @@ MPI.COMM_WORLD.Barrier()
 submesh = adios4dolfinx.read_mesh(
     MPI.COMM_WORLD, filename, engine, dolfinx.mesh.GhostMode.shared_facet
 )
-W = dolfinx.fem.functionspace(submesh, basix.ufl.element("Lagrange", "tetrahedron", 1))
-u_los_mean = dolfinx.fem.Function(W)
 U_sub = dolfinx.fem.functionspace(submesh, basix.ufl.element("Lagrange", "tetrahedron", 1))
-u_los = dolfinx.fem.Function(U_sub)
-u_mean = dolfinx.fem.Function(U_sub)
-u_los.name = "u_n_sub"
+print(U_sub.dofmap.index_map.size_local)
+print(U_sub.dofmap.index_map.num_ghosts)
+u_loss = [dolfinx.fem.Function(U_sub) for _ in range(n_outputs)]
+u_los_mean = dolfinx.fem.Function(U_sub)
 
 sub_file_vtx = dolfinx.io.VTXWriter(submesh.comm, f"./output/random_s_los/final_mean.bp", [u_los_mean], engine="BP4")
-# sub_file_vtx2 = dolfinx.io.VTXWriter(submesh.comm, f"./output/random_s_los/random_ahc_{n}/submesh_checkpoint2.bp", [u_n_sub], engine="BP4")
 
 for i in range(num_steps):
     t += dt
     if (i+1) % 20 == 0:
-        for n in range(n_outputs):
+        for u_los in u_loss:
+            u_los.name = "u_n_sub"
             filename = f'./output/random_s_los/random_ahc_{n}/submesh_checkpoint.bp'
             adios4dolfinx.read_function(u_los, filename, engine, time=t)
-            u_mean = ufl.algebra.Sum(u_mean, u_los)
-        u_mean = ufl.algebra.Division(u_mean, n_outputs)
-        u_mean_expr = dolfinx.fem.Expression(u_mean, W.element.interpolation_points())    
-        u_los_mean.interpolate(u_mean_expr)
+            n += 1
+            u_los_mean.x.array[:] += u_los.x.array
+            u_los_mean.x.scatter_forward()
+
+        n = 0
+        u_los_mean.x.array[:] /= len(u_loss)
         sub_file_vtx.write(t)
-        # sub_file_vtx2.write(t)
+
 for i in range(num_steps2):
     t += dt2
     if (i+1) % 20 == 0:
-        for n in range(n_outputs):
+        for u_los in u_loss:
+            u_los.name = "u_n_sub"
             filename = f'./output/random_s_los/random_ahc_{n}/submesh_checkpoint.bp'
             adios4dolfinx.read_function(u_los, filename, engine, time=t)
-            u_mean = ufl.algebra.Sum(u_mean, u_los)
-        u_mean = ufl.algebra.Division(u_mean, n_outputs)
-        u_mean_expr = dolfinx.fem.Expression(u_mean, W.element.interpolation_points())    
-        u_los_mean.interpolate(u_mean_expr)
-        sub_file_vtx.write(t)
-        # sub_file_vtx2.write(t)
+            n += 1
+            u_los_mean.x.array[:] += u_los.x.array
+            u_los_mean.x.scatter_forward()
 
+        n = 0
+        u_los_mean.x.array[:] /= len(u_loss)
+        sub_file_vtx.write(t)
 
 sub_file_vtx.close()
-# sub_file_vtx2.close()
