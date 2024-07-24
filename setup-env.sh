2b4dcde9963fc62f038c12f50bd1659c961c3335
Jack S. Hale <mail@jackhale.co.uk>
2024-07-24 11:32:53 +0200

    More setup.

---
+++ setup-env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+# This file is sourced at the start of job launch scripts and can be adjusted
+# to your environment.
+source $SCRATCH/spack/share/spack/setup-env.sh
+spack env activate aquifer_env
+
+# Workarounds for broken Python module find
+export PYTHONPATH=$SPACK_ENV/.spack-env/view/lib64/:$PYTHONPATH
+
+# Output useful information at start of job
+spack env status
+scontrol show job $SLURM_JOB_ID
