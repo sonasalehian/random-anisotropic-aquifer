74f86902674d4464a1e123198e6bb95f5bffc9d2
Jack S. Hale <mail@jackhale.co.uk>
2024-07-25 14:48:16 +0200

    Experiment (no FEniCS run).

--- forward_propagation/monte_carlo_launcher.sh
+++ forward_propagation/monte_carlo_launcher.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -l
 #SBATCH --job-name=monte_carlo_runner
 #SBATCH --partition batch
-#SBATCH --time=00:10:00
+#SBATCH --time=00:01:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=128
 #SBATCH --cpus-per-task=1
@@ -10,4 +10,4 @@
 source ../setup-env.sh
 ../print-env.sh
 
-parallel --jobs 4 srun -n 32 python3 single_run.py {} ::: {0..4} 
+parallel --jobs 4 "srun -N 1 -n 32 python3 -c 'from mpi4py import MPI; print(MPI.COMM_WORLD.size)'" ::: {0..3} 
