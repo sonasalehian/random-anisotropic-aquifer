ae8b059eae136ac57f1fef3b6a9fefa948b04774
Jack S. Hale <mail@jackhale.co.uk>
2024-07-26 16:19:14 +0200

    Modify to use anonymous directory.

--- setup-env.sh
+++ setup-env.sh
@@ -2,7 +2,7 @@
 # This file is sourced at the start of job launch scripts and can be adjusted
 # to your environment.
 source $HOME/spack/share/spack/setup-env.sh
-spack env activate -p aquifer_sys
+spack env activate -p ./spack_env
 
 # Workaround for broken Python module find for gmsh on uni.lu cluster
 export PYTHONPATH=$SPACK_ENV/.spack-env/view/lib64/:$PYTHONPATH
