690f9550280d615a71ca03a6ac058b5862768816
Sona Salehian <sona.salehianghamsari@uni.lu>
2024-08-19 16:16:05 +0200

    correct summary_statistics and single_run

--- forward_propagation/single_run.py
+++ forward_propagation/single_run.py
@@ -1,22 +1,28 @@
 import sys
 import os
-
+import numpy as np
 from default_parameters import create_default_parameters
 from model import solve
 
 parameters = create_default_parameters()
 
+random_folder = "random_sr"
 n = int(sys.argv[1])
 
 # Load the list of arrays
-# k_flat = np.load("../tensor_model/output/ahct_random_scaling_and_rotation.npy")
+if random_folder == "random_s":
+    k = np.load("../tensor_model/output/ahct_random_scaling.npy")
+elif random_folder == "random_r":
+    k = np.load("../tensor_model/output/ahct_random_rotation.npy")
+elif random_folder == "random_sr":
+    k = np.load("../tensor_model/output/ahct_random_scaling_and_rotation.npy")
 
 # Adjust the tansor in parameters
-# parameters["k_x_aqfr"] = k_flat[n, 0]
-# parameters["k_xy_aqfr"] = k_flat[n, 1]
-# parameters["k_yx_aqfr"] = k_flat[n, 2]
-# parameters["k_y_aqfr"] = k_flat[n, 3]
+parameters["k_x_aqfr"] = k[n, 0, 0]
+parameters["k_xy_aqfr"] = k[n, 0, 1]
+parameters["k_yx_aqfr"] = k[n, 1, 0]
+parameters["k_y_aqfr"] = k[n, 1, 1]
 
-parameters["output_dir"] = f"{os.getenv('SCRATCH')}/stochastic_model/forward_propagation/output/run_{str(n).zfill(4)}"
+parameters["output_dir"] = f"{os.getenv('SCRATCH')}/stochastic_model/forward_propagation/output/{random_folder}/run_{str(n).zfill(4)}"
 
 solve(parameters)
--- forward_propagation/summary_statistics.py
+++ forward_propagation/summary_statistics.py
@@ -1,77 +1,72 @@
-from mpi4py import MPI
-
 import adios4dolfinx
-from default_parameters import create_default_parameters
-
 import basix
 import basix.ufl
 import dolfinx
+import os
+import numpy as np
+from mpi4py import MPI
 
-parameters = create_default_parameters()
-t = parameters["t"]
-T = parameters["T"]
-num_steps = parameters["num_steps"]
-dt = T / num_steps  # time step size
-T2 = parameters["T2"]
-num_steps2 = parameters["num_steps2"]
-dt2 = T2 / num_steps2
+def count_files_starting_with(folder_path, prefix):
+    return len([f for f in os.listdir(folder_path) if f.startswith(prefix) and os.path.isfile(os.path.join(folder_path, f))])
 
-random_folder = "random_r"
-n_outputs = 3
+random_folder = "random_sr"
+
+# Number of outputs
 n_0 = 0
-filename = f"./output/{random_folder}/random_ahc_{n_0}/los_submesh_checkpoint.bp"
+folder_path = f'output/{random_folder}'
+prefix = 'run_'
+n_outputs = count_files_starting_with(folder_path, prefix)
+
+# Load time steps
+filename_output_ts = f'output/{random_folder}/run_{str(n_0).zfill(4)}/output_ts.npy'
+ts = np.load(filename_output_ts)
+
+# Read mesh
+filename = f"output/{random_folder}/run_{str(n_0).zfill(4)}/solution.bp"
 engine = "BP4"
 MPI.COMM_WORLD.Barrier()
 submesh = adios4dolfinx.read_mesh(
     MPI.COMM_WORLD, filename, engine, dolfinx.mesh.GhostMode.shared_facet
 )
 U_sub = dolfinx.fem.functionspace(submesh, basix.ufl.element("Lagrange", "tetrahedron", 1))
-print(U_sub.dofmap.index_map.size_local)
-print(U_sub.dofmap.index_map.num_ghosts)
-u_loss = [dolfinx.fem.Function(U_sub) for _ in range(n_0, n_outputs)]
-print(len(u_loss))
 u_los = dolfinx.fem.Function(U_sub)
 u_los_mean = dolfinx.fem.Function(U_sub)
+u_los_std = dolfinx.fem.Function(U_sub)
 
-sub_file_vtx = dolfinx.io.VTXWriter(
-    submesh.comm, f"./output/{random_folder}/final_mean{n_0}-{n_outputs}.bp", [u_los_mean], engine
+file_vtx_mean = dolfinx.io.VTXWriter(
+    submesh.comm, f"output/{random_folder}/final_mean{n_0}-{n_outputs}.bp", [u_los_mean], engine
 )
 
-filename_mean = f"./output/{random_folder}/mean_for_std{n_0}-{n_outputs}.bp"
-adios4dolfinx.write_mesh(submesh, filename_mean)
+file_vtx_std = dolfinx.io.VTXWriter(
+    submesh.comm, f"output/{random_folder}/final_std{n_0}-{n_outputs}.bp", [u_los_std], engine
+)
 
+for t in ts:
+    # Mean calculation
+    u_los_mean.x.array[:] = 0
+    for n in range(n_0, n_outputs):
+        u_los.name = "u_n_sub"
+        filename = f"output/{random_folder}/run_{str(n).zfill(4)}/solution.bp"
+        adios4dolfinx.read_function(u_los, filename, engine, time=t)
+        u_los_mean.x.array[:] += u_los.x.array
+        u_los_mean.x.scatter_forward()
 
-for i in range(num_steps):
-    t += dt
-    if (i + 1) % 20 == 0:
-        # print("time_step:", i)
-        u_los_mean.x.array[:] = 0
-        for n in range(n_0, n_outputs + 1):
-            # print("n:", n)
-            u_los.name = "u_n_sub"
-            filename = f"./output/{random_folder}/random_ahc_{n}/los_submesh_checkpoint.bp"
-            adios4dolfinx.read_function(u_los, filename, engine, time=t)
-            u_los_mean.x.array[:] += u_los.x.array
-            u_los_mean.x.scatter_forward()
+    u_los_mean.x.array[:] /= n_outputs - n_0
+    file_vtx_mean.write(t)
+    
+    # Standard deviation calculation
+    u_los_std.x.array[:] = 0
+    u_los_std.x.scatter_forward()
+    for n in range(n_0, n_outputs):
+        u_los.name = "u_n_sub"
+        filename = f"output/{random_folder}/run_{str(n).zfill(4)}/solution.bp"
+        adios4dolfinx.read_function(u_los, filename, engine, time=t)
+        u_los_std.x.array[:] += (u_los.x.array - u_los_mean.x.array) ** 2
+        u_los_std.x.scatter_forward()
 
-        u_los_mean.x.array[:] /= n_outputs + 1 - n_0
-        sub_file_vtx.write(t)
-        adios4dolfinx.write_function(u_los_mean, filename_mean, time=t)
+    u_los_std.x.array[:] /= n_outputs - n_0
+    u_los_std.x.array[:] = np.sqrt(u_los_std.x.array[:])
+    file_vtx_std.write(t)
 
-for i in range(num_steps2):
-    t += dt2
-    if (i + 1) % 20 == 0:
-        # print("time_step:", i)
-        u_los_mean.x.array[:] = 0
-        for n in range(n_0, n_outputs + 1):
-            u_los.name = "u_n_sub"
-            filename = f"./output/{random_folder}/random_ahc_{n}/los_submesh_checkpoint.bp"
-            adios4dolfinx.read_function(u_los, filename, engine, time=t)
-            u_los_mean.x.array[:] += u_los.x.array
-            u_los_mean.x.scatter_forward()
-
-        u_los_mean.x.array[:] /= n_outputs + 1 - n_0
-        sub_file_vtx.write(t)
-        adios4dolfinx.write_function(u_los_mean, filename_mean, time=t)
-
-sub_file_vtx.close()
+file_vtx_mean.close()
+file_vtx_std.close()
