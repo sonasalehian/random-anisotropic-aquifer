7835cf12153bb34808e0be4686941a29bfcc503d
Jack S. Hale <mail@jackhale.co.uk>
2024-09-06 08:37:10 +0200

    200 sample run done here.

--- forward_propagation/single_run.py
+++ forward_propagation/single_run.py
@@ -7,16 +7,10 @@ from model import solve
 
 parameters = create_default_parameters()
 
-random_folder = "random_s"
+random_type = "random_scaling_rotation"
 n = int(sys.argv[1])
 
-# Load the list of arrays
-if random_folder == "random_s":
-    k = np.load("../tensor_model/output/ahct_random_scaling.npy")
-elif random_folder == "random_r":
-    k = np.load("../tensor_model/output/ahct_random_rotation.npy")
-elif random_folder == "random_sr":
-    k = np.load("../tensor_model/output/ahct_random_scaling_and_rotation.npy")
+k = np.load("../tensor_model/output/ahct_{random_type}.npy")
 
 # Adjust the tansor in parameters
 parameters["k_x_aqfr"] = k[n, 0, 0]
@@ -25,7 +19,7 @@ parameters["k_yx_aqfr"] = k[n, 1, 0]
 parameters["k_y_aqfr"] = k[n, 1, 1]
 
 parameters["output_dir"] = (
-    f"{os.getenv('SCRATCH')}/stochastic_model/forward_propagation/output/{random_folder}/run_{str(n).zfill(4)}"
+    f"{os.getenv('SCRATCH')}/stochastic_model/forward_propagation/output/{random_type}/run_{str(n).zfill(4)}/"
 )
 
 solve(parameters)
