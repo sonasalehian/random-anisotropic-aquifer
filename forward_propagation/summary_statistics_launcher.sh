25110bcc61685e1820415c0a79589c9811972495
Sona Salehian Ghamsari sona.salehianghamsari@uni.lu <ssalehian@access2.aion-cluster.uni.lux>
2024-10-09 18:22:59 +0200

    run summary statistics in about 4 days

--- forward_propagation/summary_statistics_launcher.sh
+++ forward_propagation/summary_statistics_launcher.sh
@@ -1,7 +1,8 @@
 #!/bin/bash -l
 #SBATCH --job-name=summary_statistics
 #SBATCH -p batch
-#SBATCH --time=12:00:00
+#SBATCH --qos long
+#SBATCH --time=4-0:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=8
 #SBATCH --output=logs/%x-%j.out
